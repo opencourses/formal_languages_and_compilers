d9c19cf127a6a152ac5f0c662f190630c0a4392d
AntonioLangiu <antonio.langiu@studenti.polito.it>
2016-06-05 00:56:02 +0200

    start adding some useful function to the parser

--- Lab6/Ex6.1/Main.java
+++ Lab6/Ex6.1/Main.java
@@ -1,9 +1,12 @@
 import java.io.*;
 
 public class Main {
+
+    public static String file_name;
     static public void main(String argv[]){
+        file_name = argv[0];
         try {
-            scanner l = new scanner(new FileReader(argv[0]));
+            scanner l = new scanner(new FileReader(file_name));
             parser p = new parser(l);
             p.parse();
         } catch (Exception e) {
