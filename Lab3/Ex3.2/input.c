4b162e17263d03d3282d9f3b66bba84392c89f19
AntonioLangiu <antonio.langiu@studenti.polito.it>
2016-06-04 12:03:57 +0200

    add some tests

--- Lab3/Ex3.2/input.c
+++ Lab3/Ex3.2/input.c
@@ -1,4 +1,4 @@
-extern int *fn1(int a, int b, char *c[]);
+/*extern int *fn1(int a, int b, char *c[]);
 register int ff;
 
 int fn2() {
@@ -8,10 +8,44 @@ int fn2() {
 	}
 }
 
+*/
+int function () {
+    if (a == b) {
+        k = k -1;
+    } else {
+        fn2();
+    }
+    // Test the precedence
+    b = (4+5)-b[7];
+    if (3+2-a[4] < 3-3*a[c]+1 & b == 3) {
+        return ok;
+    }
+    if (a)
+        if (b)
+            print (b);
+        else
+            print (c);
+}
+
+
+int test() {
+    switch (b) {
+        case 7:
+            return 7;
+        case 9:
+            return 9;
+            break;
+        default:
+            return 10;
+    }
+}
+
 int main() {
   	char *miovett[] = {"Inverno", "Estate"};
   	while(fn1(2,3, miovett) != 0) {
-		ff++;
+		if (a)
+            continue;
+        ff++;
 	}
 	return ff;
 }
